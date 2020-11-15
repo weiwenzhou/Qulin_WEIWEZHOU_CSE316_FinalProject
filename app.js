5da70b070ccc758a67a0b8be92cee87e5f9a0bb1
wzhou2 <wzhou2@stuy.edu>
2020-11-15 17:23:40 -0500

    include front facing routes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get('/', (req, res) => {
     res.send("Welcome to index. More to come.")
 })
 
+require('./routes/routes.js')(app)
+
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
