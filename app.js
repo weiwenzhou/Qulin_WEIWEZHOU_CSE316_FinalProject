8fb2567111b229ba81f3a2c506b38be24bc0c589
wzhou2 <wzhou2@stuy.edu>
2020-11-24 01:23:10 -0500

    set up react for react on production

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express")
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Welcome to index. More to come.")
-})
+// Backend routes
+const routes = require('./routes/routes.js');
+app.use('/api', routes)
 
-require('./routes/routes.js')(app)
+app.use(express.static('client/build'))
+// connection with client setup
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("Welcome to index. More to come.")
+    })
+}
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
