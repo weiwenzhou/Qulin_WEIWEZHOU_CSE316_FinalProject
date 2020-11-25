9defa36c4b624787f6f8da139a0efb8f2f9c742b
wzhou2 <wzhou2@stuy.edu>
2020-11-24 21:20:58 -0500

    repurpose front end routes from backend

---
+++ client/src/routes.js
@@ -0,0 +1,18 @@
+import { Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+    return <Switch>
+        <Route exact path="/">Index</Route>
+        <Route exact path="/labtech">Lab login page</Route>
+        
+        <Route exact path="/employee">Employee login page</Route>
+        <Route exact path="/employee/:userId">Showing homepage for employee</Route>
+        <Route exact path="/collection/:userId">Test Collection page for employee </Route>
+        
+        <Route exact path="/lab/:userId">Lab Home page for employee</Route>
+        <Route exact path="/pool/:userId">Pool Mapping page for employee</Route>
+        <Route exact path="/well/:userId">Well Testing page for employee</Route>
+    </Switch>
+}
+
+export default Routes;
