cf74d4553beee1f319a330908f3fdf8c565821a0
wzhou2 <wzhou2@stuy.edu>
2020-11-28 19:57:17 -0500

    ignored form authentication for now and just redirect to next path

--- client/src/components/authenticationForm.js
+++ client/src/components/authenticationForm.js
@@ -14,23 +14,35 @@ class authenticationForm extends Component {
 
         this.submit = (e) => {
             e.preventDefault();
-            fetch("http://localhost:8000/api/employee", {
-                method: "POST",
-                body: this.state
-            }).then(response => {
-                console.log(response);
-            }).then(data => console.log(data));
+            // fetch("http://localhost:8000/api/employee", {
+            //     method: "POST",
+            //     body: this.state
+            // }).then(response => {
+            //     console.log(response);
+            // }).then(data => console.log(data));
+            if (this.props.action === "LabTech Login Page") {
+                this.props.parent.history.push("/lab/1");
+            } else {
+                this.props.parent.history.push("/employee/1");
+            }
         };
     }
 
     render() {
         return (
             <div>
+                <div>
+                    <h2>{this.props.action}</h2>
+                </div>
                 <form onSubmit={e => this.submit(e)}>
-                <label>Lab ID:</label>
-                <input type="text" name="id" value={this.state.id} onChange={e => this.updateForm(e)} id="id" required/>
-                <label>Password:</label>
-                <input type="password" name="password" value={this.state.password} onChange={e => this.updateForm(e)} id="password" required/>
+                <div>
+                    <label>Lab ID :</label>
+                    <input type="text" name="id" value={this.state.id} onChange={e => this.updateForm(e)} id="id" required/>
+                </div>
+                <div>
+                    <label>Password :</label>
+                    <input type="password" name="password" value={this.state.password} onChange={e => this.updateForm(e)} id="password" required/>
+                </div>
                 <button>Submit</button>
                 </form>
             </div>
