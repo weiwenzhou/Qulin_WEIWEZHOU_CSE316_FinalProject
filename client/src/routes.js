8258f5bde331e2a72be859c0487e5f611fed2a95
wzhou2 <wzhou2@stuy.edu>
2020-11-29 15:33:43 -0500

    fix labTech HomePage component

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,4 @@
 import { Switch, Route } from 'react-router-dom';
-import Homepage from './pages/homepage';
 const Login = require("./pages/login").default;
 const HomePage = require("./pages/homepage").default;
 const TestCollection = require("./pages/testCollection").default;
@@ -12,7 +11,7 @@ const Routes = () => {
         <Route exact path="/employee" render={(props) => <Login labtech={false} {...props} />} />
         <Route exact path="/employee/:userId">Showing homepage for employee</Route>
         
-        <Route exact path="/lab/:userId" component={Homepage} />
+        <Route exact path="/lab/:userId" component={HomePage} />
         <Route exact path="/collection/:userId" component={TestCollection} />
         <Route exact path="/pool/:userId">Pool Mapping page for employee</Route>
         <Route exact path="/well/:userId">Well Testing page for employee</Route>
