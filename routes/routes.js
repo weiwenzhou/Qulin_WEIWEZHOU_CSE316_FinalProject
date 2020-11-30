c4013cbb9f6a14a7176b3c8194c4739aeec99815
Queeny Lin <queeny.lin@stonybrook.edu>
2020-11-30 15:54:55 -0500

    employee login and test collection

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const app = express.Router();
+var labID;
+var empEmail;
 
 //lab tech login page
 app.get("/labtech", (req, res) => { 
     // let query = url.parse(req.url, true).query;
-    let sql = `SELECT * FROM registered 
-        WHERE labID = ` + req.body.labId + ` 
+
+    let sql = `SELECT * FROM labemployee 
+        WHERE labID = ` + req.body.labID + ` 
         AND password =` + req.body.password;
+        
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        if (result.length === 0) {
+            res.sendStatus(404);
+        }
+        else {
+            res.sendStatus(200);
+            labID = req.body.labID;
+        }
+        res.close()
+    })
+})
+
+//employee login page
+app.get("/employee", (req, res) => {
+    // res.send("temp to be deleted");
+    let sql = `SELECT * FROM employee 
+    WHERE email = ` + req.body.email + ` 
+    AND password =` + req.body.password;
+    empEmail = req.body.email;
     con.query(sql, function(err, result) {
         if (err) throw err;
         if (result.length === 0) {
@@ -19,19 +43,36 @@ app.get("/labtech", (req, res) => {
     })
 })
 
-//employee login page
-app.get("/employee", (req, res) => {
-    res.send("temp to be deleted");
-})
-
-//lab home
-app.get("/labhome", (req, res) => {
-    
-})
-
 //test collection
-app.get("/testcollection", (req, res) => {
-    
+app.post("/testcollection", (req, res) => {
+    let sql = `INSERT INTO employeetest 
+            (testBarcode, employeeID, collectionTime, collectedBy)
+        SELECT * FROM 
+            (SELECT ` + req.body.testBarcode + `, ` + req.body.employeeID + `, CURDATE(), ` + req.body.labID `)  
+            WHERE NOT EXISTS (
+                SELECT testBarcode FROM employeetest WHERE testBarcode =` + req.body.testBarcode `) LIMIT 1`;
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        let sql = `SELECT employeeID, testBarCode FROM employeetest`;
+        con.query(sql, function(err, result) {
+            if (err) throw err;
+            res.send(result);
+        })
+    })
+    res.close();
+})
+app.delete("/testcollection", (req, res) => {
+    let sql = `DELETE FROM employeetest 
+           WHERE testBarcode = ` + req.body.testBarcode `LIMIT 1`;
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        let sql = `SELECT employeeID, testBarCode FROM employeetest`;
+        con.query(sql, function(err, result) {
+            if (err) throw err;
+            res.send(result);
+        })
+    })
+    res.close();
 })
 
 //pool mapping
@@ -46,7 +87,7 @@ app.get("/welltesting", (req, res) => {
 
 //employee results page
 app.get("/employee_results", (req, res) => {
-    
+    let sql = `SELECT `;
 })
 
 module.exports = app;
